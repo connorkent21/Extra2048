7a1ca981f0794dfa4c99ce75f260b4dac528564d
Thomas <tammanle@edu.uwaterloo.ca>
2019-01-24 16:46:00 -0500

    made the grid

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,21 +2,86 @@ import React, { Component } from 'react';
 
 
 
+
 class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      gameGrid: [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ],
 
     };
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  };
+
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
   }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+      this.state.gameGrid.forEach(row => {
+
+      })
+  }
+
+  handleKeyDown(e) {
+    console.log('this is the evenT: ', e);
+  }
+
+
+
+
 
 
   render() {
     return(
       <div className='container'>
-        
+        <div className="containerGrid">
+          <div className="cubeItem">
+          </div>
+          <div className="cubeItem">
+          </div>
+          <div className="cubeItem">
+          </div>
+          <div className="cubeItem">
+          </div>
+          <div className="cubeItem">
+          </div>
+          <div className="cubeItem">
+          </div>
+          <div className="cubeItem">
+          </div>
+          <div className="cubeItem">
+          </div>
+          <div className="cubeItem">
+          </div>
+          <div className="cubeItem">
+          </div>
+          <div className="cubeItem">
+          </div>
+          <div className="cubeItem">
+          </div>
+          <div className="cubeItem">
+          </div>
+          <div className="cubeItem">
+          </div>
+          <div className="cubeItem">
+          </div>
+          <div className="cubeItem">
+          </div>
+        </div>
+
       </div>
 
     )
   }
 }
+
+
+export default Game;
