b689b57d69509c3515cc3763658ffd98ffdb1b11
Dan Robson <danr.236@gmail.com>
2019-01-25 09:36:11 -0500

    Added startGame Function

--- src/components/Game.js
+++ src/components/Game.js
@@ -15,11 +15,13 @@ class Game extends Component {
       ],
 
     }
+    this.startGame = this.startGame.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.invertArray = this.invertArray.bind(this);
     this.handleNumberMoves = this.handleNumberMoves.bind(this);
     this.revertArray = this.revertArray.bind(this);
     this.addNumber = this.addNumber.bind(this);
+    this.newNumber = this.newNumber.bind(this);
   };
 
   componentDidMount() {
@@ -35,6 +37,13 @@ class Game extends Component {
     this.handleNumberMoves();
     this.revertArray(keyPress.key);
     this.addNumber();
+  }
+
+  startGame() {
+    for(let i = 0; i < 2; i++) {
+      let index = Math.Floor(Math.random() * (this.state.gameGrid.length + 1));
+      this.state.gameGrid[parseInt(index / 4)][index % 4] = newNumber();
+    }
   }
 
   invertArray(keyPress) {
@@ -56,26 +65,26 @@ class Game extends Component {
 
     for (var i = 0; i < rotates; i++) {
       // Consider all squares one by one
-      for (let x = 0; x < this.state.length / 2; x++)
+      for (let x = 0; x < this.state[0].length / 2; x++)
       {
         // Consider elements in group of 4 in
         // current square
-        for (let y = x; y < this.state.length-x-1; y++)
+        for (let y = x; y < this.state[0].length-x-1; y++)
         {
           // store current cell in temp variable
           let temp = this.state[x][y];
 
           // move values from right to top
-          mat[x][y] = mat[y][this.state.length-1-x];
+          mat[x][y] = mat[y][this.state[0].length-1-x];
 
           // move values from bottom to right
-          mat[y][this.state.length-1-x] = mat[this.state.length-1-x][this.state.length-1-y];
+          mat[y][this.state[0].length-1-x] = mat[this.state[0].length-1-x][this.state[0].length-1-y];
 
           // move values from left to bottom
-          mat[this.state.length-1-x][this.state.length-1-y] = mat[this.state.length-1-y][x];
+          mat[this.state[0].length-1-x][this.state[0].length-1-y] = mat[this.state[0].length-1-y][x];
 
           // assign temp to left
-          mat[this.state.length-1-y][x] = temp;
+          mat[this.state[0].length-1-y][x] = temp;
         }
       }
     }
@@ -131,13 +140,18 @@ class Game extends Component {
       })
     })
 
+    let newDigit = newNumber()
+    let index = zeroList[Math.Floor(Math.random() * (zeroList.size() + 1))];
+    gameGrid[parseInt(newDigit / 4)][newDigit % 4] = newDigit;
+  }
+
+  newNumber(){
     let newNumber = 2;
     let randomNumber =  Math.random() * 9;
     if (Math.floor(randomNumber) == 8) {
       newNumber = 4;
     }
-    let index = zeroList[Math.Floor(Math.random() * (zeroList.size() + 1))];
-    gameGrid[parseInt(newNumber / 4)][newNumber % 4] = newNumber;
+    return newNumber;
   }
 
   render() {
